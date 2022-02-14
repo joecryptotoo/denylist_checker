bd3d0f0408456a6b0c98e5119a54d80a545ec5ad
Jason Garland <jgarland+joecryptotoo@gmail.com>
2022-02-13 19:13:37 -0800

    also check published denylist

--- check_denylist.py
+++ check_denylist.py
@@ -1,13 +1,21 @@
 from github import Github
-import time, os
+import time, os, requests, csv
 
 # Get a token from https://github.com/settings/tokens
 # You can do 30 requests per minute (2 second sleep time) when authenticating with a token
 # Without authentication you can do 10 requests per minute(6 second sleep time)
 github_token=os.getenv('GITHUB_TOKEN', None)
 g = Github(github_token)
-
 sleep_time = 2 if github_token else 6
+
+DENYLIST_URL='https://raw.githubusercontent.com/helium/denylist/main/denylist.csv'
+
+print("Getting denylist")
+
+res = requests.get(DENYLIST_URL)
+decoded_content = res.content.decode('utf-8')
+cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+deny_list = [hotspot[0] for hotspot in cr]
 
 print("Loading hotspots")
 with open("hotspots.txt") as file:
@@ -15,7 +23,8 @@ with open("hotspots.txt") as file:
 
 for addr in hotspots:
     print("Checking " + addr)
+    if (addr in deny_list): print ("Found {addr} in published denylist {url}".format(addr = addr, url=DENYLIST_URL))
     issues = g.search_issues("repo:helium/denylist " + addr)
     for issue in issues:
-        print("Found {addr} in {issue_url}".format(addr = addr, issue_url=issue.html_url))
+        print("Found {addr} in {url}".format(addr = addr, url=issue.html_url))
     if addr != hotspots[-1]: time.sleep(sleep_time)
